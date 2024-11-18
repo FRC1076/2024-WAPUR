f7ee172f779e6f10c5b76f52de27270c36b7a76e
Jesse Kane <jessekane66@gmail.com>
2024-11-18 14:37:39 -0500

    Began tuning PID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -108,7 +108,7 @@ public final class Constants {
 
                     public static class Control {
                         //PID Constants
-                        public static final double kP = 0.13;
+                        public static final double kP = 0.01;
                         public static final double kI = 0;
                         public static final double kD = 0;
 
