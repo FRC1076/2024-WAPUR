38c9c9ea8f2e015588ebe0e19cee367dbb961d92
1076software <1076software@gmail.com>
2024-11-23 15:59:13 -0500

    Comment out prints

--- src/main/java/frc/robot/subsystems/drive/ModuleIOHardware.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOHardware.java
@@ -139,7 +139,7 @@ public class ModuleIOHardware implements ModuleIO {
         m_driveEncoder.setPosition(0.0);
         m_driveEncoder.setPositionConversionFactor(Common.Drive.positionConversionFactor.in(Meter));
         m_driveEncoder.setVelocityConversionFactor(Common.Drive.velocityConversionFactor.in(MetersPerSecond));
-         System.out.println(m_driveEncoder.getVelocityConversionFactor());
+        //System.out.println(m_driveEncoder.getVelocityConversionFactor());
         m_driveEncoder.setMeasurementPeriod(10);
         m_driveEncoder.setAverageDepth(2);
 
